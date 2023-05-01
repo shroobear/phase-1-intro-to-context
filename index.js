d9cc03ca60652a19bd7013226122ced32b9b42c4
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-04-30 21:01:28 -0400

    makes createEmployeeRecord function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
